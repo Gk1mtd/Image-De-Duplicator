71136dfbe636761d733ae336227b90368902703c
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-14 19:14:20 +0100

    added import tkinter filedialog added button to ask for a directory

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from tkinter import *
+from tkinter import filedialog as fd
 from PIL import ImageTk, Image
-from skimage.metrics import structural_similarity
 from cv2 import *
+from skimage.metrics import structural_similarity
 
 canvas_size = (500, 300)
-tk_root = Tk() # creating tkinter root
-tk_canvas = Canvas(tk_root, width=canvas_size[0], height=canvas_size[1]) # creating canvas
-tk_canvas.pack() # marrying canvas and root
+tk_root = Tk()  # creating tkinter root
+tk_canvas = Canvas(tk_root, width=canvas_size[0], height=canvas_size[1])  # creating canvas
+tk_canvas.pack()  # marrying canvas and root
 # region ImageLoading
 # loading images, resizing them
 img_1 = Image.open("original.png")
-img_1 = img_1.resize((int(canvas_size[0]/2), int(canvas_size[1]/2),))
+img_1 = img_1.resize((int(canvas_size[0] / 2), int(canvas_size[1] / 2),))
 img_1 = ImageTk.PhotoImage(img_1)
 img_2 = Image.open("original.png")
-img_2 = img_2.resize((int(canvas_size[0]/2), int(canvas_size[1]/2),))
+img_2 = img_2.resize((int(canvas_size[0] / 2), int(canvas_size[1] / 2),))
 img_2 = ImageTk.PhotoImage(img_2)
 # endregion
 # adding images to canvas
 tk_canvas.create_image(0, 0, anchor=NW, image=img_1)
-tk_canvas.create_image((int(canvas_size[0]/2)), (int(canvas_size[1]/2)), anchor=NW, image=img_2)
+tk_canvas.create_image((int(canvas_size[0] / 2)), (int(canvas_size[1] / 2)), anchor=NW, image=img_2)
+
+
+# folderpath input
+def browse_file():
+    new_path = fd.askdirectory()
+    print(new_path)
+
+browseButton = Button(master=tk_root, text='Browse', width=6, command=browse_file)
+browseButton.pack()
+
 mainloop()
 
 # region Image comparison
