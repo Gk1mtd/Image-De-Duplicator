6e3887ea4b9c351451e32a90f7e483f1075db41c
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-14 18:53:17 +0100

    some refactoring working comparison by images with identical shape

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from skimage.metrics import structural_similarity
 from cv2 import *
 
 canvas_size = (500, 300)
-
 tk_root = Tk() # creating tkinter root
 tk_canvas = Canvas(tk_root, width=canvas_size[0], height=canvas_size[1]) # creating canvas
 tk_canvas.pack() # marrying canvas and root
+# region ImageLoading
 # loading images, resizing them
 img_1 = Image.open("original.png")
 img_1 = img_1.resize((int(canvas_size[0]/2), int(canvas_size[1]/2),))
@@ -15,18 +15,17 @@ img_1 = ImageTk.PhotoImage(img_1)
 img_2 = Image.open("original.png")
 img_2 = img_2.resize((int(canvas_size[0]/2), int(canvas_size[1]/2),))
 img_2 = ImageTk.PhotoImage(img_2)
+# endregion
 # adding images to canvas
 tk_canvas.create_image(0, 0, anchor=NW, image=img_1)
 tk_canvas.create_image((int(canvas_size[0]/2)), (int(canvas_size[1]/2)), anchor=NW, image=img_2)
-
 mainloop()
 
+# region Image comparison
+# Image comparison
 imageA = cv2.imread("original.png")
-imgA_G = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-imageB = cv2.imread("original.png")
-imgB_G = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+imageB = cv2.imread("modified.png")
 
-
-(score, diff) = structural_similarity(imgA_G, imgB_G, full=True)
-diff = (diff * 255).astype("uint8")
-print("SSIM: {}".format(score))
+(score, diff) = structural_similarity(imageA, imageB, multichannel=True, full=True)
+print("SSIM: {}" + str(score))
+# endregion
