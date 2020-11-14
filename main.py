6134e7cfb48471b83d0a6424b36b3463c37f597d
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-14 11:15:15 +0100

    added test images

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from tkinter import *
 root = Tk()
 canvas = Canvas(root, width = 300, height = 300)
 canvas.pack()
-img = PhotoImage(file="/Users/Michael/Desktop/test1.png")
+img = PhotoImage(file="test1.png")
 canvas.create_image(20,20, anchor=NW, image=img)
 mainloop()  
