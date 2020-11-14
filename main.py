5d312419327c8d00046cff0bd6327353f6b8f354
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-14 11:34:29 +0100

    reformat for imageTK createt test images

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 tk_root = Tk()
 tk_canvas = Canvas(tk_root, width=900, height=500)
 tk_canvas.pack()
-img_1 = PhotoImage(file="test1.png")
+img_1 = ImageTk.PhotoImage(Image.open("original.png"))
 tk_canvas.create_image(20, 20, anchor=NW, image=img_1)
 mainloop()
