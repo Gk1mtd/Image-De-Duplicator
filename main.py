2bc75c41ea7ef3bbe96a8ce2f6fa2132c7f86942
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-14 11:57:25 +0100

    reformat image to fit canvas

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-tk_root = Tk()
-tk_canvas = Canvas(tk_root, width=900, height=500)
-tk_canvas.pack()
-img_1 = ImageTk.PhotoImage(Image.open("original.png"))
-tk_canvas.create_image(20, 20, anchor=NW, image=img_1)
+canvas_size = (500, 300)
+
+tk_root = Tk() # creating tkinter root
+tk_canvas = Canvas(tk_root, width=canvas_size[0], height=canvas_size[1]) # creating canvas
+tk_canvas.pack() # marrying canvas and root
+
+img_og = Image.open("original.png")
+img_og = img_og.resize((canvas_size[0], canvas_size[1]),)
+img_1 = ImageTk.PhotoImage(img_og)
+
+tk_canvas.create_image(0, 0, anchor=NW, image=img_1)
 mainloop()
