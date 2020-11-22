4d69f56443a26990a9557b648a7161313d65ddf0
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-11-22 19:19:39 +0100

    * gui is an own class now

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 import datetime
-from tkinter import *
-from tkinter import filedialog as fd
-from PIL import ImageTk, Image
 from image_similarity_calculator import ImageSimilarityCalculator
+from gui import GUI
 
-# canvas_size = (500, 300)
-# tk_root = Tk()  # creating tkinter root
-# tk_canvas = Canvas(tk_root, width=canvas_size[0], height=canvas_size[1])  # creating canvas
-# tk_canvas.pack()  # marrying canvas and root
-# # region ImageLoading
-# # loading images, resizing them
-# img_1 = Image.open("original.png")
-# img_1 = img_1.resize((int(canvas_size[0] / 2), int(canvas_size[1] / 2),))
-# img_1 = ImageTk.PhotoImage(img_1)
-# img_2 = Image.open("original.png")
-# img_2 = img_2.resize((int(canvas_size[0] / 2), int(canvas_size[1] / 2),))
-# img_2 = ImageTk.PhotoImage(img_2)
-# # endregion
-# # adding images to canvas
-# tk_canvas.create_image(0, 0, anchor=NW, image=img_1)
-# tk_canvas.create_image((int(canvas_size[0] / 2)), (int(canvas_size[1] / 2)), anchor=NW, image=img_2)
-#
-#
-# # folderpath input
-# def browse_file():
-#     new_path = fd.askdirectory()
-#     print(new_path)
-#
-# browseButton = Button(master=tk_root, text='Browse', width=6, command=browse_file)
-# browseButton.pack()
-#
-# mainloop()
+
+obj_gui = GUI()
 
 start = datetime.datetime.now()
 # to calculate SSIM (similarity) of to pictures given as setImageA() and setImageB()
 obj_imageSimilarityCalculator = ImageSimilarityCalculator()
-obj_imageSimilarityCalculator.ssim_calculation("original.png", "modified.png")
+obj_imageSimilarityCalculator.ssim_calculation("original.png", "other_image.jpg")
 
 finish = datetime.datetime.now()
 print(finish-start)
