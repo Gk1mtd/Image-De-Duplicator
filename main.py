c221762111fecd9ed39e18a7d02c3187dfa0fc1a
michael.s.seitz@gmail.com <eU4Z8^d%$OJ^>
2020-12-22 17:20:55 +0100

    * Complete Overhaul of the whole GUI part. It is now inside the main.py and got rewritten completely

--- main.py
+++ main.py
@@ -1,15 +1,55 @@
 import datetime
+from tkinter import *
+from tkinter import filedialog as fd
+from PIL import ImageTk, Image
 from image_similarity_calculator import ImageSimilarityCalculator
-from gui import GUI
+import glob
 
-windowSize = (600, 400)
-# starts GUI
-obj_gui = GUI(windowSize, "Image DeDuplicator v0.1")
+# Variables and Objects
+pathToWorkingFolder = ""
+imageFilesInWorkingFolder = ""
+obj_imageSimilarityCalculator = ImageSimilarityCalculator()
 
-# start = datetime.datetime.now()
-# to calculate SSIM (similarity) of to pictures given as setImageA() and setImageB()
-# obj_imageSimilarityCalculator = ImageSimilarityCalculator()
-# obj_imageSimilarityCalculator.ssim_calculation("original.png", "other_image.jpg")
 
-# finish = datetime.datetime.now()
-# print(finish - start)
+def listOfAllImageFiles():
+    # path = 'c:\\projects\\hc2\\'
+    imageFilesInWorkingFolder = ""
+    imageFilesInWorkingFolder = [f for f in glob.glob(pathToWorkingFolder + "**/*.png", recursive=True)]
+    imageFilesInWorkingFolder += [f for f in glob.glob(pathToWorkingFolder + "**/*.jpg", recursive=True)]
+    imageFilesInWorkingFolder += [f for f in glob.glob(pathToWorkingFolder + "**/*.jpeg", recursive=True)]
+    for f in imageFilesInWorkingFolder:
+        print(f)
+
+
+def compare2Images(imageA, imageB):  # ImageA/B should be for now a path as string to the file
+    # start = datetime.datetime.now()
+    obj_imageSimilarityCalculator.ssim_calculation(imageA, imageB)
+    # finish = datetime.datetime.now()
+    # print(finish - start)
+
+
+def startSearchForDupes():
+    listOfAllImageFiles()
+
+
+# GUI
+def setPathToWorkingDirectory():
+    global pathToWorkingFolder
+    pathToWorkingFolder = fd.askdirectory()
+    print("Set Working Directory to: " + pathToWorkingFolder)
+
+
+def quitProgram(event):
+    sys.exit("ShortCut Quit: Eventmessage: " + str(event))
+
+
+tk_root = Tk()
+tk_root.bind("<Control-q>", quitProgram)  # binding shortcut ctrl+q to function quitProgram()
+tk_root.title("DeDup 0.1")
+buttonToSetPathToWorkingFolder = Button(tk_root, text="Set Path To Working Directory",
+                                        command=setPathToWorkingDirectory)
+buttonToSetPathToWorkingFolder.grid(row=0, column=0)
+buttonStartSearchForDupes = Button(tk_root, text="Start Search For Dupes", command=startSearchForDupes)
+buttonStartSearchForDupes.grid(row=1, column=0)
+
+mainloop()
