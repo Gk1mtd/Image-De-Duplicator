a3524aff5f1f19349716a73d4c9744d64c9115ce
Michael <michael.s.seitz@gmail.com>
2021-07-17 11:09:33 +0200

    * more terminal infos to progress

--- main.py
+++ main.py
@@ -79,8 +79,7 @@ def startSearchForDupes():
         except Exception as e:
             print("Resizing not possible: " + str(imageA))
         for j in range(1 + i, len(imageFilesInWorkingFolder)):
-            labelStatus.config(text="File: " + str(i + 1) + " of: " + str(len(imageFilesInWorkingFolder)) + " | " + str(imageFilesInWorkingFolder[j]))
-            tk_root.update_idletasks()
+            print("I-File: " + str(i + 1) + " of: " + str(len(imageFilesInWorkingFolder)))
             print("J-File: " + str(j + 1) + " of: " + str(len(imageFilesInWorkingFolder)))
             if not checkDictForExistingValues(imageFilesInWorkingFolder[j], image_score_dict):
                 imageB = cv2.imread(str(imageFilesInWorkingFolder[j]))
